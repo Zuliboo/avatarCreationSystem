df2644cd35989267558830b389b69dc45472d583
Zach - Design Computer <46008393+Zuliboo@users.noreply.github.com>
2019-01-07 16:12:51 -0800

    Added particles that change color to hands
    Push a or b to change particle color

---
+++ Interactions/Push to Change/Assets/Scripts/AlterParticle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlterParticle : MonoBehaviour {
+    private ParticleSystem ps;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        var main = ps.main;
+        //main.startColor = new Color(hSliderValueR, hSliderValueG, hSliderValueB, hSliderValueA);
+
+        bool buttonAPress = OVRInput.Get(OVRInput.Button.One); 
+        bool buttonBPress = OVRInput.Get(OVRInput.Button.Two); 
+
+        if (buttonAPress)
+        {
+            main.startColor = new Color(1, 0, 1, 1);
+        }
+
+        if (buttonBPress)
+        {
+            main.startColor = new Color(0, 1, 0, 1);
+        }
+    }
+}
