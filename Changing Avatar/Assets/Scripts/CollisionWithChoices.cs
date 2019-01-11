f12e173af2089019ebb31a7629350b24619871b2
Zach <zkdeocadiz@gmail.com>
2019-01-11 15:44:50 -0800

    Adding rigidbodies and scripts for colliding

---
+++ Changing Avatar/Assets/Scripts/CollisionWithChoices.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionWithChoices : MonoBehaviour { 
+private ParticleSystem ps;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        var main = ps.main;
+        if (col.gameObject.name == "WhiteParticlesSphere")
+        {
+            main.startColor = new Color(1, 0, 1, 1);
+        }
+
+        if (col.gameObject.name == "WhiteParticlesSphere")
+        {
+            main.startColor = new Color(0, 1, 0, 1);
+        }
+    }
+}
