320c9fe96e150d968bca8cbcfdde57049711ab55
Zach - Design Computer <46008393+Zuliboo@users.noreply.github.com>
2019-01-15 15:08:38 -0800

    EVERYTHING WORKS
    It's not pretty  but it works

---
+++ Push to Change/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "blue")
+        {
+            // a rigidbody tagged as "white" hits the sword
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+            Material _material = GetComponent<Renderer>().material;
+            Color _targetColor = Color.blue;
+            _material.SetColor("_EmissionColor", _targetColor);
+        }
+
+        if (col.gameObject.tag == "white")
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+            Material _material = GetComponent<Renderer>().material;
+            Color _targetColor = Color.white;
+            _material.SetColor("_EmissionColor", _targetColor);
+        }
+    }
+}
